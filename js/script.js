f45f5af44ab2ea421c03e901c4a2205d171688bb
Andrea <b.a.benvenuti3@gmail.com>
2021-02-25 15:25:36 +0100

    creato un (insicurissimo) password generator

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-alert('ciao');
+var firstName = prompt("Come ti chiami?");
+var surname = prompt("Qual'è il tuo cognome?");
+var color = prompt("Colore preferito?");
+var space = " ";
+var result = firstName + space + surname + space + color + space + "27";
+
+document.getElementById('stamp').innerHTML = result;
